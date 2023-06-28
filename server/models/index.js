9ce4e73f4ad84a5cc4d38ef4873d796e67994a90
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-06-27 20:30:26 -0400

    setup rough models, started resolvers and typeDefs

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,11 @@
-const Tech = require('./Tech');
-const Matchup = require('./Matchup');
+const User = require('./User');
+const Activity = require('./Activity');
 
-module.exports = { Tech, Matchup };
+module.exports = { User, Activity };
+
+
+
+// const Tech = require('./Tech');
+// const Matchup = require('./Matchup');
+
+// module.exports = { Tech, Matchup };
