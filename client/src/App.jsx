b8f34528e97b6950dcf80890fd8129e830577ba8
jsnicholas <jsnicholas@pm.me>
2023-06-27 20:24:22 -0400

    add daisyui configs

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import LoginPage from "./pages/loginPage";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <LoginPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
