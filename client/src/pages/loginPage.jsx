810f05b1752f78806ff71d658a1823a89f349b01
Victoria <vmcnorrill@gmail.com>
2023-06-27 21:34:24 -0400

    BUG: tailwind and daisyui not styling.

--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -3,11 +3,7 @@ import LoginCard from "../components/login/loginCard";
 function LoginPage() {
   return (
     <>
-      <section className="bg-primary container mx-auto">
-        <div className="grid grid-cols-1">
           <LoginCard />
-        </div>
-      </section>
     </>
   );
 }
