9298853fb44651ec362c2bb84b2ce08ce729c879
Victoria <vmcnorrill@gmail.com>
2023-06-29 19:01:53 -0400

    started sign up card

--- client/src/components/signupcard.js
+++ client/src/components/signupcard.js
@@ -1,17 +1,17 @@
-function LoginCard() {
+function SignupCard() {
     return (
         <div className="card h-96 w-96 bg-base-100 text-neutral-content mx-auto">
             <div className="card-body items-center text-center">
                 <h2 className="card-title">Welcome!</h2>
-                <p>Please log in.</p>
+                <p>Sign up below.</p>
                 <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs" />
                 <input type="text" placeholder="Password" className="input input-bordered w-full max-w-xs" />
                 <div className="card-actions justify-end">
-                    <button className="btn btn-neutral">Log in</button>
+                    <button className="btn btn-neutral">Sign Up</button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default LoginCard;
+export default SignupCard;
---
+++ client/src/pages/signupPage.js
@@ -0,0 +1,1 @@
+import SignupCard from '../components/signupCard';
