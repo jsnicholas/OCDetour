4c2559596bf6727a48673ecf2d20436757e1d163
jsnicholas <jsnicholas@pm.me>
2023-06-29 21:04:39 -0400

    added styles to activity page

--- client/src/pages/activityPage.js
+++ client/src/pages/activityPage.js
@@ -2,25 +2,46 @@ import ActivityCard from "../components/activityCard";
 
 function ActivityPage() {
     return (
-        <div className="container">
-            <div className="columns-1 w-96 bg-primary">
-                <ActivityCard activityTitle="Meditation"
+        <>
+            <section className="container max-w-full columns-1">
+                <ActivityCard
+                    activityTitle="Meditation"
                     activityTimer="30 minutes"
                     activityDescription="Breathing Activity"
                     activityImage="https://picsum.photos/200"
                     alt="activity image placeholder" />
-                <ActivityCard activityTitle="Drawing"
+                <ActivityCard
+                    activityTitle="Drawing"
                     activityTimer="10 minutes"
                     activityDescription="Creative Activity"
                     activityImage="https://picsum.photos/200"
                     alt="activity image placeholder" />
-                <ActivityCard activityTitle="Take a Walk"
+                <ActivityCard
+                    activityTitle="Take a Walk"
                     activityTimer="60 minutes"
                     activityDescription="Exercise Activity"
                     activityImage="https://picsum.photos/200"
                     alt="activity image placeholder" />
-            </div>
-        </div>
+                <ActivityCard
+                    activityTitle="Take a Walk"
+                    activityTimer="60 minutes"
+                    activityDescription="Exercise Activity"
+                    activityImage="https://picsum.photos/200"
+                    alt="activity image placeholder" />
+                <ActivityCard
+                    activityTitle="Take a Walk"
+                    activityTimer="60 minutes"
+                    activityDescription="Exercise Activity"
+                    activityImage="https://picsum.photos/200"
+                    alt="activity image placeholder" />
+                <ActivityCard
+                    activityTitle="Take a Walk"
+                    activityTimer="60 minutes"
+                    activityDescription="Exercise Activity"
+                    activityImage="https://picsum.photos/200"
+                    alt="activity image placeholder" />
+            </section>
+        </>
     )
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   daisyui: {
     themes: ['pastel'],
   },
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    // './src/**/*.{js,ts,jsx,tsx}',
+    './src/pages/*.{js,ts,jsx,tsx}',
+    './src/components/*.{js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
 };
