2a1b163b9658abda5b98c94891523f67aea428be
jsnicholas <jsnicholas@pm.me>
2023-06-30 16:56:09 -0400

    added routing for root and activity pages, updated dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,39 @@
+// import react libraries
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import pages for routing
 import LoginPage from './pages/loginPage';
 import ActivityPage from './pages/activityPage';
-import BreatheIcon from './components/breatheIcon';
+// import tailwind css file
 import './input.css';
+
+//import apollo libraries
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+
+//set graphql route
+const gqlLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// configure apollo
+const client = new ApolloClient({
+  // set client to use gql with httplink
+  link: gqlLink,
+  cache: new InMemoryCache(),
+});
+
 
 const App = () => {
   return (
-    <>
-      <BreatheIcon />
-      <LoginPage />
-      {/* <ActivityPage /> */}
-    </>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="activities" element={<ActivityPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 };
 export default App;
