6401f1d8b543455e4c91e702e72e90b7e3aa0202
John Kersey <johnkersey2@yahoo.com>
2023-06-29 21:08:50 -0400

    added basic auth functions

---
+++ client/src/utils/API.js
@@ -0,0 +1,28 @@
+// export const createUser = (userData) => {
+//   return fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(userData),
+//   });
+// };
+
+// export const loginUser = (userData) => {
+//   return fetch('/api/users/login', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(userData),
+//   });
+// };
+
+// export const getMe = (token) => {
+//   return fetch('/api/users/me', {
+//     headers: {
+//       'Content-Type': 'application/json',
+//       authorization: `Bearer ${token}`,
+//     },
+//   });
+// };
