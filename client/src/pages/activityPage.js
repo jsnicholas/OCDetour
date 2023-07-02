a8427ced4671377f9f35066c5a049d18540f4b29
Victoria <vmcnorrill@gmail.com>
2023-07-02 17:39:31 -0400

    side drawer fits content, but not the screen

--- client/src/pages/activityPage.js
+++ client/src/pages/activityPage.js
@@ -4,16 +4,20 @@ function ActivityPage() {
 
     return (
         <>
-            <div className="drawer lg:drawer-open">
+            <div className="navbar bg-neutral">
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
+                <a className="btn btn-ghost normal-case text-xl">OCDetour</a>
+            </div>
+            <div className=" h-full drawer lg:drawer-open" >
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
                     {/* Page content here */}
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">See Activities</label>
 
                 </div>
-                <div className="drawer-side">
+                <div className="drawer-side h-fit">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-100 h-full bg-base-200 text-base-content">
+                    <ul className="menu p-4 w-fit h-fit bg-base-200 text-base-content">
                         {/* Sidebar content here */}
                         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
                         <li><a>
@@ -21,6 +25,14 @@ function ActivityPage() {
                                 activityTitle="Meditation"
                                 activityTimer={30}
                                 activityDescription="Breathing Activity"
+                                activityImage="https://picsum.photos/200"
+                                alt="activity image placeholder" /></a></li>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
+                        <li><a>
+                            <ActivityCard
+                                activityTitle="Example"
+                                activityTimer={30}
+                                activityDescription="Example Activity"
                                 activityImage="https://picsum.photos/200"
                                 alt="activity image placeholder" /></a></li>
                         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
