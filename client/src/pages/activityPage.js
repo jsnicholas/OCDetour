0f0688ab675e909e96a177028842a113b9c559d3
Victoria <vmcnorrill@gmail.com>
2023-07-02 16:41:38 -0400

    adjusted z-index for drawer to be on top of holoCircles

--- client/src/pages/activityPage.js
+++ client/src/pages/activityPage.js
@@ -8,13 +8,14 @@ function ActivityPage() {
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
                     {/* Page content here */}
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">See Activities</label>
 
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-100 h-full bg-base-200 text-base-content">
                         {/* Sidebar content here */}
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
                         <li><a>
                             <ActivityCard
                                 activityTitle="Meditation"
@@ -22,6 +23,7 @@ function ActivityPage() {
                                 activityDescription="Breathing Activity"
                                 activityImage="https://picsum.photos/200"
                                 alt="activity image placeholder" /></a></li>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
                         <li><a>
                             <ActivityCard
                                 activityTitle="Take a Walk"
@@ -29,6 +31,7 @@ function ActivityPage() {
                                 activityDescription="Exercise Activity"
                                 activityImage="https://picsum.photos/200"
                                 alt="activity image placeholder" /></a></li>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
                         <li><a>
                             <ActivityCard
                                 activityTitle="Drawing"
