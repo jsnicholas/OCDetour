751ccba5fae0be27d4677305f035ae6876ea763c
Victoria <vmcnorrill@gmail.com>
2023-07-03 19:39:27 -0400

    centered accordian, but looks small

--- client/src/components/activities/crudActivities.js
+++ client/src/components/activities/crudActivities.js
@@ -1,36 +1,34 @@
 function CrudActivities() {
     return (
-        <>
-            <div className="join join-vertical w-full">
-                <div className="collapse collapse-arrow join-item border border-base-300">
-                    <input type="radio" name="my-accordion-4" checked="checked" />
-                    <div className="collapse-title text-xl font-medium">
-                        Click to open this one and close others
-                    </div>
-                    <div className="collapse-content">
-                        <p>hello</p>
-                    </div>
+        <section className="flex flex-wrap justify-center content-center mr-96">
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" checked="checked" />
+                <div className="collapse-title text-xl font-medium">
+                    Create a New Activity
                 </div>
-                <div className="collapse collapse-arrow join-item border border-base-300">
-                    <input type="radio" name="my-accordion-4" />
-                    <div className="collapse-title text-xl font-medium">
-                        Click to open this one and close others
-                    </div>
-                    <div className="collapse-content">
-                        <p>hello</p>
-                    </div>
-                </div>
-                <div className="collapse collapse-arrow join-item border border-base-300">
-                    <input type="radio" name="my-accordion-4" />
-                    <div className="collapse-title text-xl font-medium">
-                        Click to open this one and close others
-                    </div>
-                    <div className="collapse-content">
-                        <p>hello</p>
-                    </div>
+                <div className="collapse-content">
+                    <p>CREATE FORM HERE</p>
                 </div>
             </div>
-        </>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" />
+                <div className="collapse-title text-xl font-medium">
+                    Edit an Existing Activity
+                </div>
+                <div className="collapse-content">
+                    <p>UPDATE FORM HERE</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" />
+                <div className="collapse-title text-xl font-medium">
+                    Remove an Existing Activity
+                </div>
+                <div className="collapse-content">
+                    <p>DELETE FORM HERE</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
