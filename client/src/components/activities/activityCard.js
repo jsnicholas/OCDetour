c605b048d243e067ab83d174ce66690c4d5493bd
Victoria <vmcnorrill@gmail.com>
2023-07-09 14:17:34 -0400

    removed content double

--- client/src/components/activities/activityCard.js
+++ client/src/components/activities/activityCard.js
@@ -69,17 +69,12 @@ const ActivityCard = (props) => {
       <div className="cursor-pointer h-60 w-full md: w-fit md:h-48 mx-auto bg-white rounded-xl shadow-md overflow-hidden my-4 text-left" onClick={() => document.getElementById(activityTitle).showModal()}>
         <div className="md:flex">
           <div className="md:shrink-0" description={activityDescription}>
-            {description ()}
+            {description()}
           </div>
           <div className="p-8">
             <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{activityTimer} minutes</div>
             <p className="block mt-1 text-lg leading-tight font-medium text-black">{activityTitle}</p>
             <p className="mt-2 text-slate-500" >{activityDescription} Activity</p>
-          </div>
-          <div className="p-8">
-            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{activityTimer} minutes</div>
-            <p className="block mt-1 text-lg leading-tight font-medium text-black">{activityTitle}</p>
-            <p className="mt-2 text-slate-500">{activityDescription} Activity</p>
           </div>
         </div>
       </div>
