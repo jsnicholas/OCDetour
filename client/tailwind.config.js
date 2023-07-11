c447d20c3ff2013ff986029ee6ff3d5724488cb1
Nick Johnson <114363630+jsnicholas@users.noreply.github.com>
2023-07-11 19:33:07 -0400

    update dependencies and build scripts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  daisyui: {
-    themes: ['pastel'],
-  },
+  // daisyui: {
+  //   themes: ['pastel'],
+  // },
   content: [
     // './src/**/*.{js,ts,jsx,tsx}',
     './src/pages/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('daisyui')],
+  // plugins: [require('daisyui')],
 };
