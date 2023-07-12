ce09a16d645c61e9fd5927389472290053669de3
Nick Johnson <114363630+jsnicholas@users.noreply.github.com>
2023-07-12 11:00:27 -0400

    update login checks, add message if user has no activities

--- client/src/pages/activityPage.js
+++ client/src/pages/activityPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { ME } from '../utils/queries';
-
+import { useNavigate } from 'react-router-dom';
 import ActivityCard from "../components/activities/activityCard";
 import UserProfile from "./userProfileandStats";
 
 
 function ActivityPage() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     // const refreshPage = () => {
     //     navigate(0)
     // }
@@ -15,10 +15,11 @@ function ActivityPage() {
     //use query  to get all saved activities for the user
     const { loading, data } = useQuery(ME);
     // console.log(`User data: ${JSON.stringify(data)}`);
-
+    if (!data) {
+        navigate("/login")
+    }
     // check if the user has data, if so display it
     const activitiesData = data?.me || [];
-
 
     // if data isn't here yet, say so
     if (loading) {
@@ -34,7 +35,18 @@ function ActivityPage() {
                         <UserProfile />
                     </div>
                     <div className="p-4 grid grid-cols-1 gap-4 md:grid-cols-2 text-base-content mb-10">
-                        {activitiesData?.savedActivities?.map((activity, i) => {
+                        {/* if the user has no activities, render hero block  */}
+                        {!activitiesData && <div className="hero min-h-screen bg-base-200">
+                            <div className="hero-content text-center">
+                                <div className="max-w-md">
+                                    <h1 className="text-5xl font-bold">Welcome!</h1>
+                                    <p className="py-6">We're so glad you're here. Let's create an activity together! Click the button below to create your first activity.</p>
+                                    <button className="btn btn-primary" onClick={navigate("/createactivity")}>Create Activity</button>
+                                </div>
+                            </div>
+                        </div>}
+                        {/* if the user has activities, render them */}
+                        {activitiesData && activitiesData?.savedActivities?.map((activity, i) => {
                             return (
                                 <ActivityCard
                                     key={i}
@@ -43,7 +55,8 @@ function ActivityPage() {
                                     activityDescription={activity.activityDescription}
                                 />
                             )
-                        })}
+                        })
+                        }
                     </div>
                 </section>
             </section>
