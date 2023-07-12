2b8b8d11054fcee537bacf5fd85312655be19096
Nick Johnson <114363630+jsnicholas@users.noreply.github.com>
2023-07-12 11:17:45 -0400

    update no activities message

--- client/src/pages/activityPage.js
+++ client/src/pages/activityPage.js
@@ -37,8 +37,7 @@ function ActivityPage() {
                             <div className="hero-content text-center">
                                 <div className="max-w-md">
                                     <h1 className="text-5xl font-bold">Welcome!</h1>
-                                    <p className="py-6">We're so glad you're here. Let's create an activity together! Click the button below to create your first activity.</p>
-                                    <button className="btn btn-primary" onClick={navigate("/createactivity")}>Create Activity</button>
+                                    <p className="py-6">We're so glad you're here. Let's create an activity together! Click the edit button in the toolbar below to create your first activity.</p>
                                 </div>
                             </div>
                         </div>}
