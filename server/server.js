6a4a3d7dc8ca59d81893585ad264ea69e3e2ca90
Nick Johnson <114363630+jsnicholas@users.noreply.github.com>
2023-07-11 20:39:45 -0400

    update static path

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build/index.html')));
 }
 
 app.get('/', (req, res) => {
